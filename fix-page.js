0d218bc0868d2103f5d5a0f9ade6ed6260a5cde2
mig1098 <mig1098@hotmail.com>
2015-02-25 12:45:49 -0500

    load2

--- fix-page.js
+++ fix-page.js
@@ -1,1 +1,1 @@
-var sel,opt;sel=document.getElementById("pagelayoutfile");opt = document.createElement("option");opt.value = opt.text = "page.royalty.html";sel.add(option);
+var sel,opt;sel=document.getElementById("pagelayoutfile");opt = document.createElement("option");opt.value = opt.text = "page.royalty.html";sel.add(opt);
